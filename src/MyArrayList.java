531aedcdb2a61b1f3741bce39f8388a2c20c1d57
Aruzhan2004 <220191@astanait.edu.kz>
2023-04-16 14:25:09 +0600

    Defining variables and implementing methods add(), get(), remove(), size()

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,76 @@
-public class MyArrayList {
+public abstract class MyArrayList<T> implements MyList<T> {
+    private Object[] elements;
+    private int size;
+
+    public MyArrayList() {
+        elements = new Object[10];
+        size = 0;
+    }
+
+    private void resize() {
+        Object[] newElements = new Object[elements.length * 2];
+        System.arraycopy(elements, 0, newElements, 0, elements.length);
+        elements = newElements;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+
+    @Override
+    public void add(T item) {
+        if (size == elements.length) {
+            resize();
+        }
+        elements[size] = item;
+        size++;
+    }
+
+    @Override
+    public void add(T item, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size == elements.length) {
+            resize();
+        }
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = item;
+        size++;
+    }
+
+    @Override
+    public boolean remove(T item) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(item)) {
+                remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        T removedElement = (T) elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        size--;
+        return removedElement;
+    }
+
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) elements[index];
+    }
+
 }
