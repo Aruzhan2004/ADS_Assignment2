e6b9e472e409d57cbcc5a8bc42bb3ee2c9d3ca60
Aruzhan2004 <220191@astanait.edu.kz>
2023-04-16 15:36:55 +0600

    Defining variables and implementing methods add(), get(), remove(), size()

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,2 +1,148 @@
-public class MyLinkedList {
+public abstract class MyLinkedList<T> implements MyList<T> {
+    private class Node {
+        T element;
+        Node prev;
+        Node next;
+
+        public Node(T element, Node prev, Node next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(T item) {
+        add(item, size);
+    }
+    @Override
+    public void add(T item, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node newNode = new Node(item, null, null);
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else if (index == 0) {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        } else if (index == size) {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        } else {
+            Node curr = getNode(index);
+            newNode.prev = curr.prev;
+            newNode.next = curr;
+            curr.prev.next = newNode;
+            curr.prev = newNode;
+        }
+        size++;
+    }
+    @Override
+    public boolean remove(T item) {
+        Node current = head;
+        while (current != null) {
+            if (current.element.equals(item)) {
+                if (current == head) {
+                    head = head.next;
+                    if (head != null) {
+                        head.prev = null;
+                    } else {
+                        tail = null;
+                    }
+                } else if (current == tail) {
+                    tail = tail.prev;
+                    if (tail != null) {
+                        tail.next = null;
+                    } else {
+                        head = null;
+                    }
+                } else {
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                }
+                size--;
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        T element = current.element;
+        if (current == head) {
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            } else {
+                tail = null;
+            }
+        } else if (current == tail) {
+            tail = tail.prev;
+            if (tail != null) {
+                tail.next = null;
+            } else {
+                head = null;
+            }
+        } else {
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+        size--;
+        return element;
+    }
+
+    @Override
+    public T get(int index) {
+        return getNode(index).element;
+    }
+
+
+
+    private Node getNode(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index < size / 2) {
+            Node curr = head;
+            for (int i = 0; i < index; i++) {
+                curr = curr.next;
+            }
+            return curr;
+        } else {
+            Node curr = tail;
+            for (int i = size - 1; i > index; i--) {
+                curr = curr.prev;
+            }
+            return curr;
+        }
+    }
+
+
+
 }
